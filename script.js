c7ff202cc29c185fc5c58a8f1a88f8a66ee1f21a
ppoohh25 <poohrocker25@gmail.com>
2021-12-16 08:20:44 +0700

    1

--- script.js
+++ script.js
@@ -5,10 +5,35 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 300){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
+    //slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
     //toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    //typing animation script
+    var typed = new Typed(".typing",{
+        strings: ["Coding","Technology","Basketball","FantasyNovel"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true
+    });
+    var typed = new Typed(".typing2",{
+        strings: ["Coding","Technology","Basketball","FantasyNovel"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true
+    });
+
 });
